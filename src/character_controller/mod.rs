dd913ac15f0438984ff4e531880345f0c5dad2e2
Jakob Stechow <jakob.stechow@pm.me>
2025-10-31 10:35:24 +0100

    fix: enemies visually not on ground

--- src/character_controller/mod.rs
+++ src/character_controller/mod.rs
@@ -22,9 +22,7 @@ pub const CHARACTER_CAPSULE_LENGTH: f32 = 1.3;
 pub const CHARACTER_HEIGHT: f32 =
     CHARACTER_CAPSULE_LENGTH + CHARACTER_CAPSULE_RADIUS * 2.0;
 
-// Remember to update update_agent_target when this is no longer negative.
-pub const LOCAL_FEET_CHARACTER: f32 =
-    -((CHARACTER_CAPSULE_LENGTH + CHARACTER_CAPSULE_RADIUS * 2.0) / 2.);
+pub const LOCAL_FEET_CHARACTER: f32 = -1.0;
 
 pub const WALK_VELOCITY: f32 = 2.0;
 pub const RUN_VELOCITY: f32 = 5.0;
